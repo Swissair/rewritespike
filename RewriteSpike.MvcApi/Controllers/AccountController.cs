3cca77e31203ef6b95162c004cdc5bed0f112bb3
piotr <pskirski@hotmail.com>
2015-06-10 00:04:24 +0200

    Added account files

---
+++ RewriteSpike.MvcApi/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+
+
+namespace RewriteSpike.MvcApi.Controllers
+{
+    public class AccountController : ApiControllerBase
+    {
+        [HttpPost]
+        public HttpResponseMessage Save(StudentVm studentRequest)
+        {
+            return this.Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ RewriteSpike.MvcApi/Scripts/app/controllers/acountController.js
@@ -0,0 +1,16 @@
+﻿angular.module('registrationApp')
+.controller('AccountController', ['$scope', 'accountService', '$location', function ($scope, accountService, $location) {
+    $scope.save = function (student) {
+        $scope.error = false;
+        
+        accountService.save(student)
+        .then(function () {
+            $location.url('Registration/Courses');
+        },
+        function () {
+            alert('saving failure');
+        });
+    };
+
+
+}]);
